64e0d4a0c529ead92931689ba158dc80d0541cfe
Lucas Barros <lucaslb5656@gmail.com>
2025-02-11 17:58:52 -0400

    EWFEW

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,29 @@
+﻿using CatalogAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Models;
+
+namespace CatalogAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriasController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public CategoriasController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Categoria>> Get()
+		{
+			var categorias = _context.Categorias.ToList();
+			if (categorias is null) return NotFound("Nenhuma categoria encontrada!");
+			return categorias;
+		}
+
+
+	}
+}
